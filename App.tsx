d778242046dffad04b17782c6c9c945e148e0459
ABHISHEK44444 <153176102+ABHISHEK44444@users.noreply.github.com>
2025-07-10 15:28:43 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -16,7 +16,7 @@ export interface Permission {
     vieweeId: string;
 }
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://ai-report-manage-system-frontend.onrender.com/api';
 
 const App: React.FC = () => {
   const [currentUser, setCurrentUser] = useState<User | null>(null);
